67a27e352137724b8cb64d35a3b18ac57331827f
Matteo Sala <matteo111997@gmail.com>
2024-04-08 17:48:10 +0200

    feat: handled logic to print the api call, html config

--- vite-boolfolio/src/components/MainApp.vue
+++ vite-boolfolio/src/components/MainApp.vue
@@ -6,22 +6,31 @@ import axios from 'axios';
     name: 'MainApp',
     data(){{ 
       return{
-        arrayProjects : []
+        arrayProjects : [],
+        currentPage: '',
+        lastPage: '',
       }
      }
     },
      methods:{
-      getProjects(){
-        axios.get('http://127.0.0.1:8000/api/test')
+      getProjects(projectApiPage){
+        axios.get('http://127.0.0.1:8000/api/test',
+        {
+          params:{
+            page: projectApiPage
+          }
+        })
         .then( res=> {
-          console.log(res.data.projects);
+          // console.log(res.data.projects.data);
 
-          this.arrayProjects = res.data.projects;
+          this.arrayProjects = res.data.projects.data;
+          this.currentPage = res.data.projects.current_page;
+          this.lastPage = res.data.projects.last_page;
         })
       }
      },
      mounted(){
-      this.getProjects();
+      this.getProjects( 1 );
      }
   }
 
@@ -39,6 +48,17 @@ import axios from 'axios';
       <a href="#">{{ element.title }}</a>
     </li>
   </ul>
+
+  <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item" :class=" {'disabled': currentPage === 1}">
+      <button class="page-link btn-outline-info" @click="getProjects(currentPage-1)">Previous</button>
+    </li>
+    <li class="page-item" :class=" {'disabled': currentPage === lastPage}">
+      <button class="page-link btn-outline-info" @click="getProjects(currentPage+1)">Next</button>
+    </li>
+  </ul>
+</nav>
 
 </main>
 
