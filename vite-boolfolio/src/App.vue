62a6d9ae9650b4491746343aeabec1633415c54a
Matteo Sala <matteo111997@gmail.com>
2024-04-08 17:11:11 +0200

    first commit

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+import MainApp from './components/MainApp.vue';
+
+export default{
+    components: {
+      MainApp
+    },
+
+    data(){{ 
+      return{
+
+      }
+     }
+    },
+
+    methods:{
+      
+    },
+
+    mounted(){
+      
+    }
+}
+
+</script>
+
+<template>
+ 
+  <h1>☻ Frontend ☻</h1>
+
+  <MainApp/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-boolfolio/src/components/MainApp.vue
@@ -0,0 +1,55 @@
+<script>
+
+import axios from 'axios';
+
+  export default{
+    name: 'MainApp',
+    data(){{ 
+      return{
+        arrayProjects : []
+      }
+     }
+    },
+     methods:{
+      getProjects(){
+        axios.get('http://127.0.0.1:8000/api/test')
+        .then( res=> {
+          console.log(res.data.projects);
+
+          this.arrayProjects = res.data.projects;
+        })
+      }
+     },
+     mounted(){
+      this.getProjects();
+     }
+  }
+
+</script>
+
+<template>
+  
+<main>
+  
+  <h3>☺ Printing projects... ☺</h3>
+
+  <ul>
+    <li 
+    v-for="(element,index) in arrayProjects" :key="element.id">
+      <a href="#">{{ element.title }}</a>
+    </li>
+  </ul>
+
+</main>
+
+</template>
+
+<style scoped>
+ul{
+  li{
+    font-size: 20px;
+    line-height: 40px;
+    text-align: left;
+  }
+}
+</style>
